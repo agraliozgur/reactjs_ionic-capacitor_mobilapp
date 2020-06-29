d1efc567ddcaaf7beb6d9bac7fb109e00479dfaa
agraliozgur <zgr.agrali@gmail.com>
2020-06-29 03:13:07 +0300

    first commit

---
+++ build/precache-manifest.6100046819f371df3be74a4acdd89fba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10628721a39dce16016407f573e4852b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0ba092d681b2b37dfdd",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "91fb261eeda5e93630c0",
+    "url": "/static/js/2.950735c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.950735c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0ba092d681b2b37dfdd",
+    "url": "/static/js/main.c9ce5c5f.chunk.js"
+  },
+  {
+    "revision": "a3ae8ca4e86f9cfd2295",
+    "url": "/static/js/runtime-main.47dd04f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
